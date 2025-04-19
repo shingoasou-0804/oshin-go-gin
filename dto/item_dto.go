0dfcd4f3190a494cd20feb90c2671f13e81de38a
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-04-19 12:50:31 +0900

    modify go fmt

--- dto/item_dto.go
+++ dto/item_dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type CreateItemIput struct {
-	Name string `json:"name" binding:"required,min=2"`
-	Price uint `json:"price" binging:"required,min=1,max=999999"`
+	Name        string `json:"name" binding:"required,min=2"`
+	Price       uint   `json:"price" binging:"required,min=1,max=999999"`
 	Description string `json:"description"`
 }
