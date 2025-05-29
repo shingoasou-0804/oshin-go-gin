7bf58fc013c4ca9bfac0b29cd5199547b097cfda
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-05-29 10:16:48 +0900

    execute migration

---
+++ migrations/migration.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/shingoasou-0804/oshin-go-gin/infra"
+	"github.com/shingoasou-0804/oshin-go-gin/models"
+)
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+
+	if err := db.AutoMigrate(&models.Item{}); err != nil {
+		panic("Failed to migrate database")
+	}
+}
