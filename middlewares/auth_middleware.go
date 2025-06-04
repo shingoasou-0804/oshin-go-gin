32a8423af15f9acbf206fbbbc2cf2f9348a2cea3
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2025-06-04 09:36:37 +0900

    add auth middleware

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,33 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shingoasou-0804/oshin-go-gin/services"
+)
+
+func AuthMiddleware(authService services.IAuthService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		header := ctx.GetHeader("Authorization")
+		if header == "" {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		if !strings.HasPrefix(header, "Bearer ") {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(header, "Bearer ")
+		user, err := authService.GetUserFromToken(tokenString)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		ctx.Set("user", user)
+		ctx.Next()
+	}
+}
